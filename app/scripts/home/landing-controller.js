87aceab22cee782ca7adf284252c2464e8e896fd
fuybooo <fuybooo@qq.com>
2017-05-03 18:56:17 +0800

    跨域访问，数据库连接测试

--- app/scripts/home/landing-controller.js
+++ app/scripts/home/landing-controller.js
@@ -1,8 +1,34 @@
 define(function(require){
     var app = require('app');
-    app.controller('LandingController', function($scope, $state){
+    app.controller('LandingController', function($scope, $state, $http){
         console.log('LandingController');
-        
+        $scope.landingTest = function(){
+            // 测试跨域请求get
+            $http({
+                method: 'get',
+                url: 'http://127.0.0.1:3333/request?data1=中文参数&name=name',
+                params: {
+                    name2: '我是中文参数'
+                }
+            }).success(function(data){
+                console.log('success', data);
+            });
+        };
+
+
+        $scope.landingPost = function(){
+            $http({
+                method: 'post',
+                url: 'http://127.0.0.1:3333/save',
+                data: {
+                    id: 1,
+                    name: '我是中文参数',
+                    disabled: false
+                }
+            }).success(function(data){
+                console.log('success', data);
+            })
+        }
     });
     
 });
