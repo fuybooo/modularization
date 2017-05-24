a3139624919869938aaf5f847b61114dab4ff410
fuybooo <fuybooo@qq.com>
2017-05-24 23:53:47 +0800

    提交banner样式

--- app/scripts/fuybooo-mall/fm-banner-directive.js
+++ app/scripts/fuybooo-mall/fm-banner-directive.js
@@ -2,10 +2,9 @@ define(function (require) {
     var app = require('app'),
         $ = require('jquery');
     app.directive('sliderList', function () {
-        console.log('====================sliderListDirective')
         return {
             link: function(scope,ele,attrs){
-                var liList = $(ele).find('li');
+                var liList = $(ele).find('li.fm-banner-slider-item');
                 var i = 0;
                 var length = liList.length;
                 setInterval(function(){
@@ -13,6 +12,10 @@ define(function (require) {
                     i = (i + 1) % length;
                     $(liList[i]).fadeIn(600);
                 }, 3000);
+                
+                var prevBtn = $(ele).find('.fm-banner-slider-btn-prev');
+                var nextBtn = $(ele).find('.fm-banner-slider-btn-next');
+                
             }
         };
     })
