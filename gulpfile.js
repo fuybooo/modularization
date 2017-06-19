3bdaf19be871189f529cabad9a01c57a6696a03c
fuybooo <fuybooo@qq.com>
2017-06-19 18:34:26 +0800

    提交ng-annotate-----自动分析angularjs的推断注入

--- gulpfile.js
+++ gulpfile.js
@@ -4,114 +4,115 @@ process.env.DISABLE_NOTIFIER = true;
 var gulp = require('gulp'),
     $ = require('gulp-load-plugins')(),
     browserSync = require('browser-sync');
-
 // 压缩vendor
 gulp.task('vendor', ['clean:vendor'], function () {
-    var filterJS = $.filter('./app/vendor/**/*.js', {restore: true});
-    var filterCss = $.filter('./app/vendor/**/*.css', {restore: true});
-    return gulp.src('./app/vendor/**/*.*')
+    var filterJS = $.filter('app/vendor/**/*.js', {restore: true});
+    var filterCss = $.filter('app/vendor/**/*.css', {restore: true});
+    return gulp.src('app/vendor/**/*.*')
         .pipe(filterJS)
         .pipe($.uglify())
         .pipe(filterJS.restore)
         .pipe(filterCss)
         .pipe($.cleanCss())
         .pipe(filterCss.restore)
-        .pipe(gulp.dest('./dist/vendor'))
+        .pipe(gulp.dest('dist/vendor'))
         .pipe($.notify({message: 'vendor task complete'}));
 });
 // 清除dist/vendor
 gulp.task('clean:vendor', function () {
-    return gulp.src(['./dist/vendor/**/*.*'], {read: false})
-        .pipe($.plumber({
-            errorHandler: function (error) {
-                $.notify.onError('Error: <%= error.message %>')
-            }
-        }))
+    return gulp.src(['dist/vendor/**/*.*'], {read: false})
+        // .pipe($.plumber({
+        //     errorHandler: function (error) {
+        //         $.notify.onError('Error: <%= error.message %>')
+        //     }
+        // }))
         .pipe($.clean());
 });
 // 压缩scripts
-gulp.task("scripts", ['clean:scripts'], function () {
-    return gulp.src("./app/scripts/**/*.js")
-        .pipe($.plumber({
-            errorHandler: function (error) {
-                $.notify.onError('Error: <%= error.message %>')
-            }
-        }))
-        .pipe($.ngmin())
+gulp.task('scripts', ['clean:scripts'], function () {
+    return gulp.src('app/scripts/**/*.js')
+        // .pipe($.plumber({
+        //     errorHandler: function (error) {
+        //         $.notify.onError('Error: <%= error.message %>')
+        //     }
+        // }))
+        .pipe($.ngAnnotate())
         .pipe($.uglify())
-        .pipe(gulp.dest("./dist/scripts"))
+        .pipe(gulp.dest('dist/scripts'))
         .pipe($.notify({message: 'scripts task complete'}));
 });
+
+
 // 清除dist/scripts
 gulp.task('clean:scripts', function () {
-    return gulp.src(['./dist/scripts/**/*.js'], {read: false})
-        .pipe($.plumber({
-            errorHandler: function (error) {
-                $.notify.onError('Error: <%= error.message %>')
-            }
-        }))
+    return gulp.src(['dist/scripts/**/*.js'], {read: false})
+        // .pipe($.plumber({
+        //     errorHandler: function (error) {
+        //         $.notify.onError('Error: <%= error.message %>')
+        //     }
+        // }))
         .pipe($.clean());
 });
 // 压缩json
-gulp.task("json", ['clean:json'], function () {
-    return gulp.src("./app/json/**/*.json")
-        .pipe(gulp.dest("./dist/json"))
+gulp.task('json', ['clean:json'], function () {
+    return gulp.src('app/json/**/*.json')
+        .pipe(gulp.dest('dist/json'))
         .pipe($.notify({message: 'json task complete'}));
 });
 // 清除dist/json
 gulp.task('clean:json', function () {
-    return gulp.src(['./dist/json/**/*.json'], {read: false})
-        .pipe($.plumber({
-            errorHandler: function (error) {
-                $.notify.onError('Error: <%= error.message %>')
-            }
-        }))
+    return gulp.src(['dist/json/**/*.json'], {read: false})
+        // .pipe($.plumber({
+        //     errorHandler: function (error) {
+        //         $.notify.onError('Error: <%= error.message %>')
+        //     }
+        // }))
         .pipe($.clean());
 });
 // 压缩styles
 gulp.task('styles', ['clean:styles'], function () {
-    return gulp.src('./app/styles/**/*.css')
+    return gulp.src('app/styles/**/*.css')
         .pipe($.autoprefixer())
         .pipe($.csscomb())
         .pipe($.cleanCss())
-        .pipe(gulp.dest('./dist/styles'))
+        .pipe(gulp.dest('dist/styles'))
         .pipe($.notify({message: 'styles task complete'}));
 });
 // 清除dist/styles
 gulp.task('clean:styles', function () {
-    return gulp.src(['./dist/styles/**/*.css'], {read: false})
-        .pipe($.plumber({
-            errorHandler: function (error) {
-                $.notify.onError('Error: <%= error.message %>')
-            }
-        }))
+    return gulp.src(['dist/styles/**/*.css', 'dist/styles/*.css'], {read: false})
+        // .pipe($.plumber({
+        //     errorHandler: function (error) {
+        //         $.notify.onError('Error: <%= error.message %>')
+        //     }
+        // }))
         .pipe($.clean());
 });
 // 压缩images
 gulp.task('images', ['clean:images'], function () {
-    return gulp.src('./app/images/**/*.*')
-        .pipe($.plumber({
-            errorHandler: function (error) {
-                $.notify.onError('Error: <%= error.message %>')
-            }
-        }))
+    return gulp.src('app/images/*.*')
+        // .pipe($.plumber({
+        //     errorHandler: function (error) {
+        //         $.notify.onError('Error: <%= error.message %>')
+        //     }
+        // }))
         .pipe($.cache($.imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
-        .pipe(gulp.dest('./dist/images'))
+        .pipe(gulp.dest('dist/images'))
         .pipe($.notify({message: 'images task complete'}));
 });
 // 清除dist/images
 gulp.task('clean:images', function () {
-    return gulp.src(['./dist/images/**/*.*'], {read: false})
-        .pipe($.plumber({
-            errorHandler: function (error) {
-                $.notify.onError('Error: <%= error.message %>')
-            }
-        }))
+    return gulp.src(['dist/images/**/*.*'], {read: false})
+        // .pipe($.plumber({
+        //     errorHandler: function (error) {
+        //         $.notify.onError('Error: <%= error.message %>')
+        //     }
+        // }))
         .pipe($.clean());
 });
 // 压缩views
 gulp.task('views', ['clean:views'], function () {
-    return gulp.src('./app/views/**/*.html')
+    return gulp.src('app/views/**/*.html')
         .pipe($.htmlmin({
             collapseWhitespace: true,
             collapseBooleanAttributes: true,
@@ -122,32 +123,32 @@ gulp.task('views', ['clean:views'], function () {
             minifyJS: true,
             minifyCSS: true
         }))
-        .pipe(gulp.dest('./dist/views'))
+        .pipe(gulp.dest('dist/views'))
         .pipe($.notify({message: 'images task complete'}));
 });
 // 清除dist/views
 gulp.task('clean:views', function () {
-    return gulp.src(['./dist/views/**/*.*'], {read: false})
-        .pipe($.plumber({
-            errorHandler: function (error) {
-                $.notify.onError('Error: <%= error.message %>')
-            }
-        }))
+    return gulp.src(['dist/views/**/*.*'], {read: false})
+        // .pipe($.plumber({
+        //     errorHandler: function (error) {
+        //         $.notify.onError('Error: <%= error.message %>')
+        //     }
+        // }))
         .pipe($.clean());
 });
 // 生成template 缓存模块
 gulp.task('template', function(){
-    return gulp.src('./app/views/**/*.html')
+    return gulp.src('app/views/**/*.html')
         .pipe($.angularTemplatecache())
-        .pipe(gulp.dest('./app/scripts/template-cache'))
+        .pipe(gulp.dest('app/scripts/template-cache'))
 });
 
 var copyA2B = function (A, B) {
     var dirs = ['images', 'json', 'scripts', 'styles', 'vendor', 'views'];
     for (var i = 0, l = dirs.length; i < l; i++) {
         var dir = dirs[i],
-            start = './' + A + '/' + dir + '/**/*.*',
-            end = './' + B + '/' + dir;
+            start = '' + A + '/' + dir + '/**/*.*',
+            end = '' + B + '/' + dir;
         gulp.src(start)
             .pipe(gulp.dest(end))
             .pipe($.notify({message: 'copy ' + dir + ' complete'}));
@@ -155,21 +156,21 @@ var copyA2B = function (A, B) {
 };
 // clean app
 gulp.task('clean:app', function () {
-    return gulp.src(['./app/**/*.*'], {read: false})
+    return gulp.src(['app/**/*.*'], {read: false})
         .pipe($.clean());
 });
 // clean .temp
 gulp.task('clean:temp', function () {
-    return gulp.src(['./.temp/**/*.*'], {read: false})
+    return gulp.src(['.temp/**/*.*'], {read: false})
         .pipe($.clean());
 });
 // clean dist
 gulp.task('clean:dist', function () {
-    return gulp.src(['./dist/**/*.*'], {read: false})
+    return gulp.src(['dist'], {read: false})
         .pipe($.clean());
 });
 gulp.task('jshint', function () {
-    return gulp.src('./app/scripts/**/*.js')
+    return gulp.src('app/scripts/**/*.js')
         .pipe($.jshint())
         .pipe($.jshint.reporter('default'));
 });
@@ -178,25 +179,25 @@ gulp.task('jshint', function () {
 gulp.task('serve', function () {
     browserSync.init({
         open: false,
-        server: './'
+        server: ''
     });
     gulp.watch(['app/**/*.*', 'test/private-test/*.html']).on('change', browserSync.reload);
 });
 // 监听任务
 gulp.task('watch', function () {
-    gulp.watch('./app/images/**/*.*', ['images']);
-    gulp.watch('./app/json/*.json', ['json']);
-    gulp.watch('./app/scripts/**/*.js', ['jshint', 'scripts']);
-    gulp.watch('./app/styles/**/*.css', ['styles']);
-    gulp.watch('./app/vendor/**/*.*', ['vendor']);
-    gulp.watch('./app/views/**/*.html', ['views']);
+    gulp.watch('app/images/**/*.*', ['images']);
+    gulp.watch('app/json/*.json', ['json']);
+    gulp.watch('app/scripts/**/*.js', ['jshint', 'scripts']);
+    gulp.watch('app/styles/**/*.css', ['styles']);
+    gulp.watch('app/vendor/**/*.*', ['vendor']);
+    gulp.watch('app/views/**/*.html', ['views']);
 
 });
 gulp.task('watch:ts', function(){
-    gulp.watch('./app/scripts/research/*.ts', ['ts']);
+    gulp.watch('app/scripts/research/*.ts', ['ts']);
 });
 // gulp.task('watch:ts', function(){
-//     gulp.watch('./tss/**/*.ts', ['ts']);
+//     gulp.watch('tss/**/*.ts', ['ts']);
 // });
 
 var tsProject = $.typescript.createProject('tsconfig.json')
@@ -221,7 +222,7 @@ gulp.task('build', function () {
 });
 
 // copy app -- 备份app
-gulp.task("bak", ['clean:temp'], function () {
+gulp.task('bak', ['clean:temp'], function () {
     return copyA2B('app', '.temp');
 });
 
